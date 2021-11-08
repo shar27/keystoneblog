4299682a7d296e67b13d3e0d82a991359e4466fa
shar27 <shariq.ahmed731@gmail.com>
2021-11-08 21:04:37 +0100

    icons

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,18 +1,22 @@
 import Link from 'next/link'
 import YouTubeIcon from '@material-ui/icons/Youtube';
 import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/Linkedin';
 
 
 export default function Footer() {
     return (
         <div className="container d-flex justify-content-center text-center pb-4">
-            <Link href="#">
+           
             <YouTubeIcon/>
-            </Link>
+            
                 
-            <Link href="#">
+            
             <FacebookIcon/>
-            </Link>
+            
+           
+            <LinkedInIcon/>
+            
             
         </div>
     )
